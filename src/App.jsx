3e62ce2d14950880344b3e2269ef489dfd8a63e2
Satyam Thakur <sat.thakur20@gmail.com>
2024-03-14 14:57:09 +0530

    Creating a card component with image and details

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-orange-500 text-xl mb-2 w-full text-center">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            258
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            300
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
